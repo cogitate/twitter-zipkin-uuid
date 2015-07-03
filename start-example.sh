7c006bc0dac40aa4660880a2b67920e652268ca9
Jiajun Chen <jchen65@L-SB817GFFT4-M.local>
2015-07-02 20:58:39 -0700

    change to use sqlite in memory

--- start-example.sh
+++ start-example.sh
@@ -1,1 +1,2 @@
-./bin/sbt "set offline := true" "zipkin-example/run -log.level=DEBUG -genSampleTraces=true"
+./sbt "set offline := true" "zipkin-example/run -log.level=DEBUG -genSampleTraces=true -zipkin.storage.anormdb.install=true -zipkin.storage.anormdb.db=sqlite::memory"
+#./sbt "set offline := true" "zipkin-example/run -log.level=DEBUG -genSampleTraces=true -zipkin.storage.anormdb.db=sqlite::memory"
